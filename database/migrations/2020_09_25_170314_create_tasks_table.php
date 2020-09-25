76028337ab1f793a31d2ca3735376cb0a1e54338
Tosin Moronfolu <tosinmoronfolu@gmail.com>
2020-09-25 19:06:44 +0100

    🔧 update task migration file

--- database/migrations/2020_09_25_170314_create_tasks_table.php
+++ database/migrations/2020_09_25_170314_create_tasks_table.php
@@ -15,6 +15,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('track');
+            $table->integer('level');
             $table->timestamps();
         });
     }
